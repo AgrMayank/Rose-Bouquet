48bf7b1d398c7d53ce55faecb59dd0e8bb128958
Mayank Raj <AgrMayank@outlook.com>
2020-02-06 20:40:04 +0530

    Add Native SDK

---
+++ Assets/Scripts/TakeScreenShot.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+// Generate a screenshot and save to disk with the name SomeLevel.png.
+
+public class TakeScreenShot : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        ScreenCapture.CaptureScreenshot("Rose Bouquet");
+    }
+}
